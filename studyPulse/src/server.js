6cbab5caf1371e147f63aa0158fac461c895fdad
Kim <162593303+TIMrsaa@users.noreply.github.com>
2026-05-26 15:41:37 +0900

    Delete studyPulse directory

--- studyPulse/src/server.js
+++
@@ -1,7 +0,0 @@
-/**
- * StudyPulse 서버 진입점
- * LocalServerManager가 `node src/server.js`로 이 파일을 실행합니다.
- */
-const path = require('path');
-process.chdir(path.join(__dirname, '../server'));
-require('../server/src/app');
